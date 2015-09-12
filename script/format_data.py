ed58dbcf6b8669425ae7cb4532527271c206beca
yilinhe <yilinhe.dev@gmail.com>
2015-09-12 14:00:37 -0400

    formatted.txt

--- script/format_data.py
+++ script/format_data.py
@@ -1,7 +1,7 @@
 __author__ = 'yilinhe'
 from feature_dictionary import *
 from sets import Set
-
+import random
 def format_training_data():
     with open("../data/coupon_detail_train.csv") as fin:
         fout = open("formatted_train.txt", "wb")
@@ -11,8 +11,9 @@ def format_training_data():
             item_features = item_dict[purchase[5].replace('\n', "")]
             item_count = purchase[0]
             features = [item_count, 0, len(user_features), len(item_features)] + user_features + item_features
+            #features = [item_count, 0, 1, len(item_features),user_features[0]] + item_features
             features = [str(i) for i in features]
-            fout.write("\t".join(features) + '\n')
+            fout.write(" ".join(features) + '\n')
         fout.close()
 
 
@@ -21,8 +22,9 @@ def format_testing_data():
         for user_features in user_dict.values():
             for item_features in test_item_dict.values():
                 features = [0, 0, len(user_features), len(item_features)] + user_features + item_features
+                #features = [0, 0, 1, len(item_features),user_features[0]] + item_features
                 features = [str(i) for i in features]
-                fout.write("\t".join(features) + '\n')
+                fout.write(" ".join(features) + '\n')
         fout.close()
 
 
@@ -39,9 +41,8 @@ def format_training_from_visit():
             item_count = purchase[0]
             features = [item_count, 0, len(user_features), len(item_features)] + user_features + item_features
             features = [str(i) for i in features]
-            fout.write("\t".join(features) + '\n')
+            fout.write(" ".join(features) + '\n')
         fout.close()
-
 
 def format_training_from_visit_purchase():
     with open("../data/coupon_visit_train.csv") as fin:
@@ -68,8 +69,17 @@ def format_training_from_visit_purchase():
                         elif times > 1:
                             rating = 3
                         features = [rating, 0, len(user_features), len(item_features)] + user_features + item_features
+                        #features = [rating, 0, 1, len(item_features),user_features[0]] + item_features
                         features = [str(i) for i in features]
-                        fout.write("\t".join(features) + '\n')
+                        fout.write(" ".join(features) + '\n')
+                    for item_hash in item_dict:
+                        if random.random() > 0.001:
+                            continue
+                        if item_hash not in items_visited:
+                            item_features = item_dict[item_hash]
+                            features = [0, 0, len(user_features), len(item_features)] + user_features + item_features
+                            features = [str(i) for i in features]
+                            fout.write(" ".join(features) + '\n')               
 
                 items_visited = {}
                 item_purchased = Set()
@@ -88,10 +98,14 @@ def format_training_from_visit_purchase():
 
 def format_implicit():
     with open("../data/coupon_visit_train.csv") as fin:
-        fout = open("formatted_implicit.txt", "wb")
+        train_out = open("formatted_implicit_train.txt", "wb")
+        test_out = open("formatted_implicit_test.txt", "wb")
+
         last_user_hash = 0
-        items_visited = {}
-        total = 0
+        train_visited = {}
+        test_visited = {}
+        total_train = 0
+        total_test = 0
         for row in fin.readlines()[1:]:
             purchase = row.split(',')
             user_hash = purchase[5]
@@ -99,38 +113,54 @@ def format_implicit():
             if item_hash not in item_dict:
                 continue
             if user_hash != last_user_hash:
-                if total > 0:
+                if last_user_hash !=0:
                     user_id = user_dict[last_user_hash][0].split(":")[0]
-                    features = [user_id, len(items_visited)]
-                    features += [str(item) + ":" + str(float(cnt) / total) for item, cnt in items_visited.items()]
-                    fout.write("\t".join([str(i) for i in features]) + '\n')
-                items_visited = {}
-                total = 0
+                    # train
+                    features = [len(item_dict), len(train_visited)]
+                    features += [str(item) + ":" + str(float(cnt) / total_train) for item, cnt in train_visited.items()]
+                    train_out.write(" ".join([str(i) for i in features]) + '\n')
+
+                    # test
+                    features = [len(test_visited), len(test_visited)]
+                    features += [str(item) + ":" + str(float(cnt) / total_train) for item, cnt in test_visited.items()]
+                    test_out.write(" ".join([str(i) for i in features]) + '\n')                    
+                
+                train_visited = {}
+                test_visited = {}
+                total_train = 0
+                total_test = 0
                 last_user_hash = user_hash
 
             if item_hash in test_item_dict:
+                print " test data"
                 item_id = test_item_dict[item_hash][0].split(":")[0]
+                total_test += 1
+                if item_id in test_visited:
+                    test_visited[item_id] += 1
+                else:
+                    test_visited[item_id] = 1
             if item_hash in item_dict:
                 item_id = item_dict[item_hash][0].split(":")[0]
-            if item_id in items_visited:
-                items_visited[item_id] += 1
-            else:
-                items_visited[item_id] = 1
-            total += 1
-        fout.close()
+                total_train += 1
+                if item_id in train_visited:
+                    train_visited[item_id] += 1
+                else:
+                    train_visited[item_id] = 1
+        train_out.close()
+        test_out.close()
 
 
 def print_dict():
     f = open("item_features.txt", "wb")
     for key, val in item_feature_dict.items():
-        f.write(str(key) + "\t" + "\t".join(val) + "\n")
+        f.write(str(key) + " " + " ".join(val) + "\n")
     f.close()
     f = open("user_features.txt", "wb")
     for key, val in user_dict.items():
-        f.write(str(key) + "\t" + str(val) + "\n")
+        f.write(str(key) + " " + str(val) + "\n")
     f.close()
 
 
 format_training_from_visit_purchase()
-#format_testing_data()
+format_testing_data()
 format_implicit()
--- script/format_result.py
+++ script/format_result.py
@@ -7,20 +7,22 @@ def rank_result():
     f = open("pred.txt")
     data = open("formatted_test.txt")
     fout = open("submit.txt","wb")
+    fout.write('USER_ID_hash,PURCHASED_COUPONS\n')
+
     last_id = 0
     p = []
     for pred in f.readlines():
-        info = data.readline().split("\t")
+        info = data.readline().split(" ")
+       #print info
         user_id = info[4].split(':')[0]
         if last_id != user_id:
-            if len(p ) > 0:
-                top_k = [i[1] for i in sorted(p, key=itemgetter(1), reverse = True)[:10]]
-                fout.write(reverse_user_dict[int(user_id)]+","+ " ".join([str(i).replace('\n',"") for i in top_k])+"\n")
+            if len(p) > 0:
+                top_k = [i[0] for i in sorted(p, key=itemgetter(1), reverse = True)[:10]]
+                fout.write(reverse_user_dict[int(last_id)]+","+ " ".join([str(i).replace('\n',"") for i in top_k])+"\n")
             p=[]
             last_id=user_id
         item_hash = reverse_item_dict[int(info[4+int(info[2])].split(':')[0])]
         p.append([item_hash,pred])
-
     top_k = [i[0] for i in sorted(p, key=itemgetter(1), reverse = True)[:10]]
     fout.write(reverse_user_dict[int(user_id)]+","+ " ".join([str(i).replace('\n',"") for i in top_k])+"\n")
     fout.close()
