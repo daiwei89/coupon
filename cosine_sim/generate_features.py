2da88d1b4c552f994ebc4074eba57e819f528364
daiwei89 <daiwei89@gmail.com>
2015-09-30 00:44:18 -0400

    update generate_features.py

--- cosine_sim/generate_features.py
+++ cosine_sim/generate_features.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 from collections import defaultdict
 num_user_features = 52
 num_item_features = 137
@@ -43,8 +45,80 @@ def load_dictionary_simple():
             features = [float(i.replace('"',"")) for i in user_info[1:]]
             user_dict[user_info[0].replace('"', "")] = features
 
+def load_sda_feature():
+  #train = pd.read_csv('../trans_data/sda_train_coupon.dat', header=-1)
+  #test = pd.read_csv('../trans_data/sda_test_coupon.dat', header=-1)
+
+  train = pd.read_csv('../trans_data/sda_train_coupon_lr3.000000_3.000000.dat', header=-1)
+  test = pd.read_csv('../trans_data/sda_test_coupon_lr3.000000_3.000000.dat', header=-1)
+
+  # standardize train and test to [0, 1].
+  train_mat = train.ix[:,2:].values
+  test_mat = test.ix[:,2:].values
+  print 'train_mat shape:', train_mat.shape
+  print 'test_mat shape:', test_mat.shape
+  num_train = train_mat.shape[0]
+  num_test = test_mat.shape[0]
+  combined = np.r_[train_mat, test_mat]
+  combined = combined - np.min(combined, axis=0)
+  combined = combined / np.max(combined, axis=0)
+
+  for i, t in enumerate(train.itertuples()):
+    coupon_hash = t[1]
+    coupon_small_area = t[2]
+    train_item_dict[(coupon_hash, coupon_small_area)] = list(combined[i,:])
+  print 'coupon_dim:', len(train_item_dict.itervalues().next())
+  for i, t in enumerate(test.itertuples()):
+    coupon_hash = t[1]
+    coupon_small_area = t[2]
+    test_item_dict[(coupon_hash, coupon_small_area)] = \
+        list(combined[i + num_train, :])
+  print '# train (coupons, small_area) pairs:', len(train_item_dict)
+  print '# test (coupons, small_area) pairs:', len(test_item_dict)
+
+def load_loc_feature():
+  train = pd.read_csv('../data/factor_training_coupon_loc.txt', sep='\t')
+  test = pd.read_csv('../data/factor_testing_coupon_loc.txt', sep='\t')
+  for i in range(len(train.columns)):
+    # -2 because we drops coupon_hash and coupon_small_area.
+    item_heading[train.columns[i]] = i - 2
+
+  # standardize train and test to [0, 1].
+  train_mat = train.ix[:,2:].values
+  test_mat = test.ix[:,2:].values
+  print 'train_mat shape:', train_mat.shape
+  print 'test_mat shape:', test_mat.shape
+  num_train = train_mat.shape[0]
+  num_test = test_mat.shape[0]
+  combined = np.r_[train_mat, test_mat]
+  combined = combined - np.min(combined, axis=0)
+  combined = combined / np.max(combined, axis=0)
+
+  """
+  for i in xrange(num_train):
+    coupon_hash = train.ix[i, 0]
+    coupon_small_area = train.ix[i, 1]
+    train_item_dict[(coupon_hash, coupon_small_area)] = list(combined[i,:])
+  for i in xrange(num_test):
+    coupon_hash = test.ix[i, 0]
+    coupon_small_area = test.ix[i, 1]
+    test_item_dict[(coupon_hash, coupon_small_area)] = \
+        list(combined[i + num_train,:])
+  """
+  for i, t in enumerate(train.itertuples()):
+    coupon_hash = t[1]
+    coupon_small_area = t[2]
+    train_item_dict[(coupon_hash, coupon_small_area)] = list(combined[i,:])
+  print 'coupon_dim:', len(train_item_dict.itervalues().next())
+  for i, t in enumerate(test.itertuples()):
+    coupon_hash = t[1]
+    coupon_small_area = t[2]
+    test_item_dict[(coupon_hash, coupon_small_area)] = \
+        list(combined[i + num_train, :])
+  print '# train (coupons, small_area) pairs:', len(train_item_dict)
+  print '# test (coupons, small_area) pairs:', len(test_item_dict)
+
 def GetTrainTestPurchase(test_coupon_list):
-    # train: USER_ID_hash --> [purchased_item1, purchased_item2, ...]
     train_purchase = defaultdict(list)
     test_purchase = defaultdict(list)  # similar to test.
     with open("../data/coupon_detail_train.csv") as fin:
@@ -53,10 +127,38 @@ def GetTrainTestPurchase(test_coupon_list):
             #item_count = int(purchase[0])
             user = purchase[4]
             item = purchase[5]
+            small_area_name = purchase[2]
             if item not in test_coupon_list:
                 train_purchase[user].append(item)
             else:
                 test_purchase[user].append(item)
     return train_purchase, test_purchase
 
-load_dictionary_simple()
+def GetTrainTestPurchaseAreaAug(test_coupon_list):
+    # train: (USER_ID_hash, small_area_name) -->
+    # [purchased_item1, purchased_item2, ...]
+    train_purchase = defaultdict(list)
+    test_purchase = defaultdict(list)  # similar to test.
+    coupon_detail = pd.read_csv("../data/coupon_detail_train.csv")
+    column_idx = dict(zip(coupon_detail.columns, \
+        xrange(len(coupon_detail.columns))))
+    num_train_purchases = 0
+    num_test_purchases = 0
+    for t in coupon_detail.itertuples():
+      # +1 because t[0] is row index.
+      user = t[column_idx['USER_ID_hash'] + 1]
+      item = t[column_idx['COUPON_ID_hash'] + 1]
+      small_area_name = t[column_idx['SMALL_AREA_NAME'] + 1]
+      if item not in test_coupon_list:
+          train_purchase[user].append((item, small_area_name))
+          num_train_purchases += 1
+      else:
+          test_purchase[user].append(item)
+          num_test_purchases += 1
+    print 'Read ', num_train_purchases, ' train purchases (', \
+        len(train_purchase), ' (users,small_area) pairs) and ',\
+        num_test_purchases, ' test purchases (', len(test_purchase),\
+        'users)'
+    return train_purchase, test_purchase
+
+#load_dictionary_simple()
